6cb80465a1f72485ca2401a12e9444a7e2686e81
Alex Harvey <alexharv074@gmail.com>
2019-01-02 23:13:14 +1100

    Cleanup in install.sh

--- install.sh
+++ install.sh
@@ -2,29 +2,32 @@
 
 if [ ! -d git/home/dotfiles ] ; then
   cat <<EOF
+  In the wrong directory?
 Usage:
 cd $HOME
 mkdir -p git/home
 git clone https://github.com/alexharv074/dotfiles.git git/home/dotfiles
 bash git/home/dotfiles/install.sh
 EOF
+  exit 1
 fi
 
-for i in $(
-  find git/home/dotfiles \
-    -not \( -path git/home/dotfiles/.git -prune \) \
-    -not \( -path git/home/dotfiles/install.sh -prune \) \
-    -type f
-  ) ; do
+files=$(find git/home/dotfiles \
+  -not \( -path git/home/dotfiles/.git -prune \) \
+  -not \( -path git/home/dotfiles/install.sh -prune \) \
+  -type f)
 
-  path=$(dirname $(echo $i | sed -e 's|git/home/dotfiles/||'))
+for i in $files ; do
+  path="$(
+    dirname "$(echo $i | sed -e 's!git/home/dotfiles/!!')"
+  )"
 
-  if [ "$path" == "." ] ; then
-    ln -s $i
-  else
+  if [ ! "$path" == "." ] ; then
     oldpwd=$(pwd)
     cd $path && ln -s $oldpwd/$i
     cd $oldpwd
+    continue
   fi
 
+  ln -s $i
 done
